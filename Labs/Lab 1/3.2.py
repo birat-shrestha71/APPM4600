a8c4a1716be3fe90eb8a032bf178c7249dc34de8
birat <shresthabirat71@gmail.com>
2026-09-03 10:51:17 -0600

    added second comment to 3.2.py

--- Labs/Lab 1/3.2.py
+++ Labs/Lab 1/3.2.py
@@ -13,7 +13,7 @@ print('the first three entries are of x are', x2)
 w = 10**(-np.linspace(1,10,10))
 #the entries of w are 10 ^ -1 through -10 power, which should be 0.1 growing to 1e-10
 
-x3 = np.linspace(1,10,10) 
+x3 = np.linspace(1,10,10) #sets up x axis with spacing as 1-10
 
 s = 3 * w
 
