99e2c63740920adb95a3696516957fe5c60307ea
Birat <shresthabirat71@gmail.com>
2026-09-03 22:09:38 -0600

    added homework1 content

--- Homework/HW1/HW1.py
+++ Homework/HW1/HW1.py
@@ -1,2 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt 
 
+x = np.linspace(1.92,2.080,161)
+
+p1 = x**9 - 18 * x**8 + 144 * x**7 - 672 * x**6 + 2016 * x**5 - 4032 * x**4 + 5376 * x**3 - 4608 * x**2 + 2304 * x - 512
+p2 = (x-2)**9
+
+plt.plot(x,p1)
+plt.plot(x,p2)
+
+plt.xlabel("x")
+plt.ylabel("p(x)")
+
+plt.show()
 
